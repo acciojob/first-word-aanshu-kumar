5deb8a0032e934c2630b118f7dd735cd98eed598
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-18 19:05:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
   // your code here
+	let str=s.trim();
+	let arr=str.split(" ");
+	return arr[0];
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
+
 alert(firstWord(s));
